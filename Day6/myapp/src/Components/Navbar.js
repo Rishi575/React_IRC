ce6028ed513deee18853468ad9ae18b09dcbe2b2
Rishi575 <rishichidory@gmail.com>
2023-12-14 11:22:36 +0530

    readme,Day6

---
+++ Day6/myapp/src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import '../Assests/css/Nav.css'
+import { Link } from 'react-router-dom';
+const Nav = () => {
+    return (
+        <>
+           Nav
+        </>
+    )
+}
+
+export default Nav;
---
+++ Day6/myapp/src/pages/Home.js
@@ -0,0 +1,10 @@
+import '../Assests/css/Home.css'
+const Home =()=>{
+    return(
+        <>
+            Home
+        </>
+    )
+}
+
+export default Home
